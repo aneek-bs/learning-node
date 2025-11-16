fbbeb046b0ee155c1a331934a5148f6386a3848e
aneek-bs <aneekbiswas.kol@gmail.com>
2025-11-16 17:59:11 +0530

    First commit

---
+++ 1-node-farm/index.js
@@ -0,0 +1,105 @@
+const fs = require('fs'); //Access to reading and writing data into file system
+const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const slugify = require('slugify');
+
+//FILES---------------------------------------------//
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); //Synchrononos version of file reading
+// console.log(textIn);
+
+// const textOut = `This is what we know about avocado - ${textIn}.\nCreated on ${Date.now()} `;
+// fs.writeFileSync('./txt/output.txt', textOut); //Writing the text from JS into a new text file
+// console.log('File has been written!');
+
+//This is a synchronous way of coding - each line waits for the previous line to complete.
+//This is also known as blocking code.
+
+//Alternative - Asynchronous/non-blocking code
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) {
+//     return console.log('ERROR!!!');
+//   }
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written!');
+//       });
+//     });
+//   });
+// });
+
+// console.log('Outside the Read File');
+
+//SERVERS---------------------------------------------//
+
+//Reading the contents of HTMLs and storing as a string
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProd = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+//Reading the product JSON
+const data = fs.readFileSync(`${__dirname}/dev_data/data.json`, 'utf-8'); //Synchronous as we need it to be run only once in beginning to fetch the data
+const prodData = JSON.parse(data);
+
+const slugs = prodData.map((el) => slugify(el.productName, { lower: true }));
+//console.log(slugs); //convert the product names into URL-able names
+
+//Creating the server
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true); //Fetch the pathName and the query
+
+  //Overview Page
+  if (pathname === '/overview' || pathname === '/') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+
+    const cardsHtml = prodData
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    //Product Page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    const product = prodData[query.id]; //Find the product corresponding to the ID in the URL
+    const output = replaceTemplate(tempProd, product); //Replace the placeholders with the appropriate fields from that product
+    res.end(output);
+
+    //API
+  } else if (pathname === '/api') {
+    //When we are inside /api route, we send in the JSON result
+    res.writeHead(200, {
+      'content-type': 'application/json',
+    });
+    res.end(data);
+
+    //Error Handling
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text-html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page Not Found.</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on Port 8000....');
+});
